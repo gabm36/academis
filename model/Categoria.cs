80f27af1bf7055f3f365e500e0091f5c9343e5c9
Gabriel Marques <gabriel.mjhu@gmail.com>
2023-11-22 18:44:46 -0300

    criação de models;

---
+++ model/Categoria.cs
@@ -0,0 +1,45 @@
+﻿namespace model
+{
+    public class Categoria
+    {
+        private int id = 0;
+        private string descricao = "";
+
+        public Categoria() { }
+
+        public Categoria(int id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("O id não pode ser menor do que 0.");
+                else
+                    id = value;
+            }
+        }
+
+        public string Descricao
+        {
+            get { return descricao; }
+            set
+            {
+                if (value.Length == 0)
+                    throw new ArgumentException("A descrição não pode ter um valor vazio.");
+                else if (value.Length > 50)
+                    throw new ArgumentException("A descrição não pode ter mais de 50 caracteres.");
+                else
+                    descricao = value;
+            }
+        }
+    }
+}
