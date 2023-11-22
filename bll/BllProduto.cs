26e7636df2aed317a4be451c15010f0f16186103
Gabriel Marques <gabriel.mjhu@gmail.com>
2023-11-22 19:34:42 -0300

    criação das blls;

---
+++ bll/BllProduto.cs
@@ -0,0 +1,34 @@
+﻿using dal;
+using model;
+using System.Data;
+
+namespace bll
+{
+    public class BllProduto
+    {
+        public static Produto Selecionar(int id)
+        {
+            return DalProduto.Selecionar(id);
+        }
+
+        public static void Inserir(Produto p)
+        {
+            DalProduto.Inserir(p);
+        }
+
+        public static void Atualizar(Produto p)
+        {
+            DalProduto.Atualizar(p);
+        }
+
+        public static void Deletar(int id)
+        {
+            DalProduto.Deletar(id);
+        }
+
+        public static DataTable Listar()
+        {
+            return DalProduto.Listar();
+        }
+    }
+}
